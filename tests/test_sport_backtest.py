262f06a2c6927e15dd899710f031ed027b469205
Optimalize.it <postmaster@optimalize.it>
2024-09-14 02:27:24 +0200

    added kelly

--- tests/test_sport_backtest.py
+++ tests/test_sport_backtest.py
@@ -2,9 +2,8 @@ import pytest
 import pandas as pd
 import numpy as np
 from sklearn.dummy import DummyClassifier
-from src.backtest.sport_backtest import BaseBacktest, ModelBacktest, PredictionBacktest
-from src.strategies.sport_strategies import FixedStake
-
+from backtestbuddy.backtest.sport_backtest import BaseBacktest, ModelBacktest, PredictionBacktest
+from backtestbuddy.strategies.sport_strategies import FixedStake, KellyCriterion
 
 
 class TestModelBacktest:
@@ -22,6 +21,14 @@ class TestModelBacktest:
     @pytest.fixture
     def dummy_model(self):
         return DummyClassifier(strategy="most_frequent")
+
+    @pytest.fixture
+    def kelly_strategy(self):
+        return KellyCriterion(downscaling=1)
+
+    @pytest.fixture
+    def fractional_kelly_strategy(self):
+        return KellyCriterion()
 
     @pytest.fixture
     def backtest(self, sample_data, dummy_model):
@@ -52,6 +59,142 @@ class TestModelBacktest:
         assert backtest.bookie_results is not None
         assert len(backtest.detailed_results) > 0
         assert len(backtest.bookie_results) > 0
+
+    def test_fixed_stake_strategy(self, sample_data, dummy_model):
+        backtest = ModelBacktest(
+            data=sample_data,
+            odds_columns=['odds_1', 'odds_2'],
+            outcome_column='outcome',
+            date_column='date',
+            model=dummy_model,
+            initial_bankroll=1000,
+            strategy=FixedStake(stake=100)
+        )
+        backtest.run()
+        assert backtest.detailed_results is not None
+        assert 'bt_stake' in backtest.detailed_results.columns
+        assert (backtest.detailed_results['bt_stake'] == 100).all()
+
+    def test_kelly_criterion_strategy(self, sample_data, dummy_model, kelly_strategy):
+        backtest = ModelBacktest(
+            data=sample_data,
+            odds_columns=['odds_1', 'odds_2'],
+            outcome_column='outcome',
+            date_column='date',
+            model=dummy_model,
+            initial_bankroll=1000,
+            strategy=kelly_strategy
+        )
+        backtest.run()
+        assert backtest.detailed_results is not None
+        assert 'bt_stake' in backtest.detailed_results.columns
+        assert not (backtest.detailed_results['bt_stake'] == 0).all()
+        assert (backtest.detailed_results['bt_stake'] <= backtest.detailed_results['bt_starting_bankroll']).all()
+
+    def test_fractional_kelly_strategy(self, sample_data, dummy_model, fractional_kelly_strategy):
+        backtest = ModelBacktest(
+            data=sample_data,
+            odds_columns=['odds_1', 'odds_2'],
+            outcome_column='outcome',
+            date_column='date',
+            model=dummy_model,
+            initial_bankroll=1000,
+            strategy=fractional_kelly_strategy
+        )
+        backtest.run()
+        assert backtest.detailed_results is not None
+        assert 'bt_stake' in backtest.detailed_results.columns
+        assert not (backtest.detailed_results['bt_stake'] == 0).all()
+        assert (backtest.detailed_results['bt_stake'] <= 0.5 * backtest.detailed_results['bt_starting_bankroll']).all()
+
+    def test_strategy_comparison(self, sample_data, dummy_model, kelly_strategy, fractional_kelly_strategy):
+        strategies = [
+            FixedStake(stake=100),
+            kelly_strategy,
+            fractional_kelly_strategy
+        ]
+        results = []
+
+        for strategy in strategies:
+            backtest = ModelBacktest(
+                data=sample_data,
+                odds_columns=['odds_1', 'odds_2'],
+                outcome_column='outcome',
+                date_column='date',
+                model=dummy_model,
+                initial_bankroll=1000,
+                strategy=strategy
+            )
+            backtest.run()
+            final_bankroll = backtest.detailed_results['bt_ending_bankroll'].iloc[-1]
+            results.append((strategy.__class__.__name__, final_bankroll))
+
+        assert len(results) == 3
+        assert all(isinstance(r[1], (int, float)) for r in results)
+
+    def test_model_probabilities(self, sample_data):
+        class ProbabilityDummyClassifier(DummyClassifier):
+            def predict_proba(self, X):
+                return np.random.random((len(X), 2))
+
+        prob_model = ProbabilityDummyClassifier(strategy="stratified")
+        backtest = ModelBacktest(
+            data=sample_data,
+            odds_columns=['odds_1', 'odds_2'],
+            outcome_column='outcome',
+            date_column='date',
+            model=prob_model,
+            initial_bankroll=1000,
+            strategy=KellyCriterion()
+        )
+        backtest.run()
+        assert backtest.detailed_results is not None
+        assert 'bt_model_prob_0' in backtest.detailed_results.columns
+        assert 'bt_model_prob_1' in backtest.detailed_results.columns
+
+    def test_kelly_criterion_bet_sizing(self, sample_data, dummy_model, kelly_strategy):
+        backtest = ModelBacktest(
+            data=sample_data,
+            odds_columns=['odds_1', 'odds_2'],
+            outcome_column='outcome',
+            date_column='date',
+            model=dummy_model,
+            initial_bankroll=1000,
+            strategy=kelly_strategy
+        )
+        backtest.run()
+        stakes = backtest.detailed_results['bt_stake']
+        bankrolls = backtest.detailed_results['bt_starting_bankroll']
+        assert (stakes >= 0).all()  # Kelly should never suggest negative stakes
+        assert (stakes <= bankrolls).all()  # Kelly should never suggest betting more than the bankroll
+
+    def test_fractional_kelly_vs_full_kelly(self, sample_data, dummy_model, kelly_strategy, fractional_kelly_strategy):
+        full_kelly_backtest = ModelBacktest(
+            data=sample_data,
+            odds_columns=['odds_1', 'odds_2'],
+            outcome_column='outcome',
+            date_column='date',
+            model=dummy_model,
+            initial_bankroll=1000,
+            strategy=kelly_strategy
+        )
+        fractional_kelly_backtest = ModelBacktest(
+            data=sample_data,
+            odds_columns=['odds_1', 'odds_2'],
+            outcome_column='outcome',
+            date_column='date',
+            model=dummy_model,
+            initial_bankroll=1000,
+            strategy=fractional_kelly_strategy
+        )
+        full_kelly_backtest.run()
+        fractional_kelly_backtest.run()
+        
+        full_kelly_stakes = full_kelly_backtest.detailed_results['bt_stake']
+        fractional_kelly_stakes = fractional_kelly_backtest.detailed_results['bt_stake']
+        
+        assert (fractional_kelly_stakes <= full_kelly_stakes).all()
+
 
 class TestPredictionBacktest:
     @pytest.fixture

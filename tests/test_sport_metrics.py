96f3eaf7903217a24e3b694f36e3d69301f111b0
Optimalize.it <postmaster@optimalize.it>
2024-09-14 02:27:37 +0200

    updated imports

--- tests/test_sport_metrics.py
+++ tests/test_sport_metrics.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 import numpy as np
-from src.metrics.sport_metrics import *
+from backtestbuddy.metrics.sport_metrics import *
 
 @pytest.fixture
 def sample_data():

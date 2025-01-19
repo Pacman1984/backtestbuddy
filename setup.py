646700cee5aa7622d39ae32fcb0baedb9c912cc3
Optimalize.it <postmaster@optimalize.it>
2025-01-19 19:22:28 +0100

    chore: bump version to 0.1.7 in setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="backtestbuddy",
-    version="0.1.5",
+    version="0.1.7",
     author="Sebastian Pachl",
     description="A flexible backtesting framework for trading and betting strategies",
     long_description=long_description,
